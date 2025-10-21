75c37944300e28233f9bcaf7290cb72da4523694
Ningkang-ZHAO <156332521+Ningkang-ZHAO@users.noreply.github.com>
2025-10-21 08:16:02 +0000

    Deploying to gh-pages from @ Ningkang-ZHAO/Ningkang-ZHAO.github.io@0a88835614e3612ee3f73d503ee31a2166d4c640 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-some-rambling-about-my-phd-application",
+        
+          title: "Some rambling about my PhD application",
+        
+        description: "some rambling",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/PhDApplication/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
